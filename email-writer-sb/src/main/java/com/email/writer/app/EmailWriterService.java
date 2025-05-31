7e4e07a19ebb5d89162ecfe833e7dfbf24d8f0d8
jumanakj48 <kalimathjumana.cs21@bitsathy.ac.in>
2025-05-31 22:39:45 +0530

    Removed unused import

--- email-writer-sb/src/main/java/com/email/writer/app/EmailWriterService.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailWriterService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.List;
 import java.util.Map;
 
 @Service
